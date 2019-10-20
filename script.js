0e9c1de1d6b9c93a61805efb237e557010c1b975
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-10-19 21:07:35 -0600

    clock works

---
+++ script.js
@@ -0,0 +1,25 @@
+function updateCss() {
+  let d = new Date();
+  console.log(d)
+  seconds = ((d.getSeconds()/60)*360)+90;
+  minutes = d.getMinutes()/60*360+90;
+  hours = d.getHours()/12*360+90 ;
+
+
+  console.log(seconds, minutes, hours)
+  //update position of hands to match
+
+  document.getElementsByClassName(
+    "secondHand"
+  )[0].style.transform = `rotate(${seconds}deg)`;
+  document.getElementsByClassName(
+    "minuteHand"
+  )[0].style.transform = `rotate(${minutes}deg)`;
+  document.getElementsByClassName(
+    "hourHand"
+  )[0].style.transform = `rotate(${hours}deg)`;
+}
+
+setInterval(() => {
+  updateCss();
+}, 1000);
